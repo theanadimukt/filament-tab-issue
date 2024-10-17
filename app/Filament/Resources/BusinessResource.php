d9cf7a2550e856cecbd09270a12c8c8f6dc5bbb6
sagar.patel <sagarpatel9697@gmail.com>
2024-10-17 21:27:41 +0100

    Initial commit

---
+++ app/Filament/Resources/BusinessResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BusinessResource\Pages;
+use App\Filament\Resources\BusinessResource\RelationManagers;
+use App\Models\Business;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class BusinessResource extends Resource
+{
+    protected static ?string $model = Business::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Tabs::make('Tabs')
+                    ->tabs([
+                        Tabs\Tab::make('Tab 1')
+                            ->schema([
+                                Section::make('Sunday')
+                                    ->schema([
+                                        //
+                                    ]),
+                                Section::make('Monday')
+                                    ->schema([
+                                        //
+                                    ]),
+                                Section::make('Tuesday')
+                                    ->schema([
+                                        //
+                                    ]),
+                            ])
+                            ->columns(3),
+                        // ...
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBusinesses::route('/'),
+            'create' => Pages\CreateBusiness::route('/create'),
+            'edit' => Pages\EditBusiness::route('/{record}/edit'),
+        ];
+    }
+}
